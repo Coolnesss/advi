213af20dad088fae85ca32f3ea5e76db70574203
Chang <chang.rajani@gmail.com>
2018-11-20 16:57:01 +0200

    improvements

--- advi.py
+++ advi.py
@@ -5,31 +5,37 @@ from torch.autograd import Variable
 class ADVI:
     def __init__(self, M=1, ):
         self.M = M
-        pass
 
     def learn(self, posterior_func, n_params=5, q_dist="mean"):
 
         mu = Variable(torch.zeros(n_params), requires_grad=True)
-        omega = Variable(torch.zeros(n_params), requires_grad=True)
+        omega = Variable(torch.ones(n_params), requires_grad=True)
+        
+        print("starting mu and omega", mu, omega)
 
-        for i in range(1000):
+        for i in range(3000):
+            epsilon = torch.randn(n_params)
+            xi = (1.0 / omega) * (epsilon - mu)
 
-            epsilon = torch.randn(self.M, n_params)
-            xi = (1/omega) * (epsilon - mu)
-            theta = xi # TODO replace with real transformation
+            theta = Variable(xi, requires_grad=True) # TODO replace with real transformation
+            print("theta",theta)
+            posterior = posterior_func(theta)
+            posterior.backward()
 
-            posterior = posterior_func(theta).sum(1)
+            mu_gradient = theta.grad
+            
+            # note formula, GRADIENT is given in eq 8!
+            omega_gradient = (mu_gradient * (epsilon @ torch.diag(torch.exp(omega)))) + 1
 
-            print("posterior mean", posterior.mean())
-            mu_gradient = torch.autograd.grad(posterior.mean(), mu)
-            (epsilon @ torch.diag(torch.exp(omega)))
-            omega_grad_part = (posterior * (epsilon @ torch.diag(torch.exp(omega)))).mean() + 1
-            omega_gradient = torch.autograd.grad(omega_grad_part, omega)
+            mu = mu + torch.tensor([1e-4]) * mu_gradient
+            omega = omega + torch.tensor([1e-4]) * omega_gradient
 
+            theta.grad.zero_()
+            assert theta.grad == 0
 
-            mu += 1e-3 * mu_gradient
-            omega += 1e-3 * omega_gradient
+            if i % 500 == 0:
+                print("mu now", mu, "sigma", omega)
 
-        print(mu)
+        print("ended with", mu)
 
 
--- test.py
+++ test.py
@@ -5,17 +5,30 @@ from torch import nn
 import matplotlib.pyplot as plt
 import numpy as np 
 from torch import distributions
-from torch.distributions import Bernoulli
+from torch.distributions import Bernoulli, Normal
 
 def log_reg_test():
     X, y = data.create_logreg_dataset()
 
     def posterior_func(theta):
-        return Bernoulli(torch.sigmoid(X @ theta.t())).log_prob(y)
+        return Bernoulli(torch.sigmoid(theta.squeeze() @ X.t())).log_prob(y).sum() + Normal(0, 1).log_prob(theta).sum()
     
+    print("asd",posterior_func(torch.randn(9)).shape)
     m = advi.ADVI()
     m.learn(posterior_func, n_params=9)
+
+
+def gaussian_test():
+    n_samples = 1000
+    data = Normal(5, 1).sample((n_samples, ))
+
+    def posterior_func(theta):
+        return Normal(theta, 1).log_prob(data).sum() + Normal(5, 5).log_prob(theta).sum()
     
+    m = advi.ADVI()
+    m.learn(posterior_func, n_params=1)
+
 if __name__ == '__main__':
 
-    log_reg_test()
+    torch.set_default_dtype(torch.float64)
+    gaussian_test()
